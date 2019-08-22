6ff81d18d052c314d8b0548cf67cb53dbc652542
Akshay <akshaycm@hotmail.com>
2019-08-22 21:55:10 +0300

    add -y

--- setupdeps.sh
+++ setupdeps.sh
@@ -2,7 +2,7 @@ cd ~
 git clone https://github.com/akshaynexus/Node-Monitor
 sudo apt-get install curl -y
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt-get install nodejs
+sudo apt-get install nodejs -y
 npm i express bitcoin-rpc-promise dotenv forever
 cp .quantisnetcore/quantisnet.conf ~/Node-Monitor/.env
 cd ~/Node-Monitor
