5ad171343c6b23b99e53a85c2a2278d3fc6d5d9d
Akshay <akshaycm@hotmail.com>
2019-08-22 23:07:36 +0300

    update to proper env fields

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 
 /* Declare local variables */
 const auth = {
-  user: process.env.rpcusername,
+  user: process.env.rpcuser,
   pass: process.env.rpcpassword,
   host: "localhost", /* Default Recommended */
   port: 9796
